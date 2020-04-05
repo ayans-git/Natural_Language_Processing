44fbd6910e398e62f74157ee4cad1c94eb193737
ayans-git <57875525+ayans-git@users.noreply.github.com>
2020-04-04 22:23:22 -0400

    Update PorterStemmedText.py

--- PorterStemmedText.py
+++ PorterStemmedText.py
@@ -1,5 +1,11 @@
 from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
 
 def PorterStemmedText(text):
     stemmer = PorterStemmer()
-    return stemmer.stem(text)
+    words = word_tokenize(text)
+    stemmed_text = []
+    for word in words:
+        stemmed_text.append(stemmer.stem(word))
+        stemmed_text.append(", ")
+    return "".join(stemmed_text)
